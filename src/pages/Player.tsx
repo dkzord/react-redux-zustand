e6821e5bfa49afa2cfb97213662f758082a93ef7
Fernando Cavalcanti <comododk@gmail.com>
2026-01-28 00:46:25 -0300

    feat: refactor application structure and integrate Tailwind CSS with new components

---
+++ src/pages/Player.tsx
@@ -0,0 +1,50 @@
+import { MessageCircle } from 'lucide-react';
+import { Header } from '../components/Header';
+import { Module } from '../components/Module';
+import { Video } from '../components/Video';
+
+export function Player() {
+  return (
+    <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
+      <div className="flex w-[1100px] flex-col gap-6">
+        <div className="flex items-center justify-between">
+          <Header />
+
+          <button className="flex items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white hover:bg-violet-600 transition-colors">
+            <MessageCircle className="w-4 h-4" />
+            Deixar feedback
+          </button>
+        </div>
+
+        <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow pr-80">
+          <div className="flex-1">
+            <Video />
+          </div>
+
+          <aside className="w-80 absolute top-0 bottom-0 right-0 border-l border-zinc-800 bg-zinc-900 divide-y-2 divide-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
+            <Module
+              title="Introdução ao Redux"
+              amountOfLessons={10}
+              moduleIndex={0}
+            />
+            <Module
+              title="Fundamentos do Redux"
+              amountOfLessons={15}
+              moduleIndex={1}
+            />
+            <Module
+              title="Redux Toolkit"
+              amountOfLessons={20}
+              moduleIndex={2}
+            />
+            <Module
+              title="Redux Persist"
+              amountOfLessons={25}
+              moduleIndex={3}
+            />
+          </aside>
+        </main>
+      </div>
+    </div>
+  );
+}
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,25 +1,10 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import type { TypedUseSelectorHook } from 'react-redux';
 import { useSelector } from 'react-redux';
 
-const todoSlice = createSlice({
-  name: 'todo',
-  initialState: ['fazer café', 'estudar React', 'Zustand'],
-
-  reducers: {
-    add(state, action) {
-      state.push(action.payload);
-    },
-  },
-});
-
 export const store = configureStore({
-  reducer: {
-    todo: todoSlice.reducer,
-  },
+  reducer: {},
 });
-
-export const { add } = todoSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
